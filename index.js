efc651d165bde093221532a160d066e57a2785cf
hadil <hihijazi@gmail.com>
2023-11-10 19:39:31 -0500

    save changes complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+console.log(customerName);
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+console.log(upperCaseCustomerName);
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(newFavorite) {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'me';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'me';
+}
